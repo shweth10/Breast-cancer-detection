f05047e40f6833e4fc21407e19137d04dcfc9182
shweth10 <91576928+shweth10@users.noreply.github.com>
2023-04-04 22:40:38 +1200

    user display

--- resources/views/layouts/main-template.blade.php
+++ resources/views/layouts/main-template.blade.php
@@ -80,7 +80,7 @@
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-          <img src="{{ asset('AdminLTE-3.1.0/dist/img/user2-160x160.jpg') }}" class="img-circle elevation-2" alt="User Image">
+          <img src="{{ asset('AdminLTE-3.1.0/dist/img/3d-illustration-person-with-sunglasses_23-2149436188.avif') }}" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
           <a class="d-block">
