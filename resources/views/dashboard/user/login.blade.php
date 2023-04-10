515aeb8d5007120b072ac2c4cb2fc1132907436a
shweth10 <91576928+shweth10@users.noreply.github.com>
2023-04-10 12:13:21 +1200

    updated view of login and register page

--- resources/views/dashboard/user/login.blade.php
+++ resources/views/dashboard/user/login.blade.php
@@ -6,16 +6,52 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>User Login</title>
     <link rel="stylesheet" href="{{ asset('bootstrap.min.css') }}">
-    <link rel="stylesheet" href="\css\loginstyles.css">
+    <link rel="stylesheet" href="\css\tailwind.css">
+    <link rel="stylesheet" href="\css\index.css">
+    <link rel="stylesheet" href="\css\all.min.css">
 </head>
-<body>
-
+<body class="text-blueGray-700 antialiased">
+<nav
+      class="top-0 absolute z-50 w-full flex flex-wrap items-center justify-between px-2 py-3 navbar-expand-lg"
+    >
+      <div
+        class="container px-4 mx-auto flex flex-wrap items-center justify-between"
+      >
+        <div
+          class="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start"
+        >
+          <a
+            class="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white"
+            href="/"
+            >Car Policy Hub</a
+          ><button
+            class="cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
+            type="button"
+            onclick="toggleNavbar('example-collapse-navbar')"
+          >
+            <i class="text-white fas fa-bars"></i>
+          </button>
+        </div>
+       
+    </nav>
     
+    <main>
+      <section class="relative w-full h-full py-40 min-h-screen">
+        <div
+          class="absolute top-0 w-full h-full bg-blueGray-800 bg-full bg-no-repeat"
+          style="background-image: url(/images/register_bg_2.png)"
+        ></div>
+        <div class="container mx-auto px-4 h-full">
+          <div class="flex content-center items-center justify-center h-full">
+            <div class="w-full lg:w-4/12 px-4">
+              <div
+                class="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-blueGray-200 border-0"
+              >
 
-    <div class="container">
-        <div class="row">
-            <div class="login" style="margin-top: -250px;" >
-                  <h1>User Login</h1><hr>
+                <div class="flex-auto px-4 lg:px-10 py-10 pt-0">
+                  <div class="text-blueGray-400 text-center mb-3 font-bold">
+                    <small>User Login</small>
+                  </div>
                   <form action="{{ route('user.check') }}" method="post" autocomplete="off">
                     @if (Session::get('fail'))
                         <div class="alert alert-danger">
@@ -27,29 +63,132 @@
                     <div class="alert alert-info">
                         {{ Session::get('info') }}
                     </div>
-                @endif
+                    @endif
+
                     @csrf
-                      <div class="form-group">
-                          <label for="email"/* Styling the label. */
-                          style="color: white; font-weight: bold;">Email</label>
-                          <input type="text" class="form-control" name="email" placeholder="Enter email address" value="{{ Session::get('verifiedEmail') ? Session::get('verifiedEmail') : old('email') }}">
-                          <span class="text-danger">@error('email'){{ $message }}@enderror</span>
-                      </div>
-                      <div class="form-group">
-                          <label for="password"style="color: white; font-weight: bold;">Password</label>
-                          <input type="password" class="form-control" name="password" placeholder="Enter password" value="{{ old('password') }}">
-                          <span class="text-danger">@error('password'){{ $message }}@enderror</span>
-                      </div>
-                      <a href="{{ route('user.forgot.password.form') }}">Forgot password</a>
-                      <div class="form-group mt-2">
-                          <button type="submit" class="btn btn-primary">Login</button>
-                      </div>
-                      <br>
-                      <a href="{{ route('user.register') }}">Create new Account</a>
-                  </form>
+                    <div class="relative w-full mb-3">
+                      <label
+                        class="block uppercase text-blueGray-600 text-xs font-bold mb-2"
+                        for="email"
+                        >Email</label
+                      ><input
+                        type="email"
+                        name="email"
+                        class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                        placeholder="Enter email address"
+                        value="{{ Session::get('verifiedEmail') ? Session::get('verifiedEmail') : old('email') }}"
+                      />
+                      <span class="text-danger">@error('email'){{ $message }}@enderror</span>
+                    </div>
+                    <div class="relative w-full mb-3">
+                      <label
+                        class="block uppercase text-blueGray-600 text-xs font-bold mb-2"
+                        for="password"
+                        >Password</label
+                      ><input
+                        type="password"
+                        name="password"
+                        class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                        placeholder="Enter password"
+                        value="{{ old('password') }}"
+                      />
+                      <span class="text-danger">@error('password'){{ $message }}@enderror</span>
+                    </div>
+                    <div class="text-center mt-6">
+                      <button
+                        class="bg-blueGray-800 text-white active:bg-blueGray-600 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full ease-linear transition-all duration-150"
+                        type="submit"
+                      >
+                        Sign In
+                      </button>
+                    </div>
+                    <div class="flex flex-wrap mt-6">
+                <div class="w-1/2">
+                  <a href="{{ route('user.forgot.password.form') }}" 
+                    ><small>Forgot password?</small></a
+                  >
+                </div>
+                <div class="w-1/2 text-right">
+                  <a href="{{ route('user.register') }}" 
+                    ><small>Create new account</small></a
+                  >
+                </div>
+              </div>
             </div>
+          </div>
         </div>
-    </div>
-    
-</body>
+                  </form>
+                </div>
+              </div>
+        <footer class="absolute w-full bottom-0 bg-blueGray-800 pb-6">
+          <div class="container mx-auto px-4">
+            <hr class="mb-6 border-b-1 border-blueGray-600" />
+            <div
+              class="flex flex-wrap items-center md:justify-between justify-center"
+            >
+              <div class="w-full md:w-4/12 px-4">
+                <div
+                  class="text-sm text-white font-semibold py-1 text-center md:text-left"
+                >
+                  Copyright © <span id="get-current-year"></span>
+                  <a
+                    href="/"
+                    class="text-white hover:text-blueGray-300 text-sm font-semibold py-1"
+                    >Car Policy Hub</a
+                  >
+                </div>
+              </div>
+              <div class="w-full md:w-8/12 px-4">
+                <ul
+                  class="flex flex-wrap list-none md:justify-end justify-center"
+                >
+                  <li>
+                    <a
+                      href="/"
+                      class="text-white hover:text-blueGray-300 text-sm font-semibold block py-1 px-3"
+                      >Group 1</a
+                    >
+                  </li>
+                  <li>
+                    <a
+                      href="/"
+                      class="text-white hover:text-blueGray-300 text-sm font-semibold block py-1 px-3"
+                      >About Us</a
+                    >
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </footer>
+      </section>
+    </main>
+  </body>
+  <script src="https://unpkg.com/@popperjs/core@2/dist/umd/popper.js"></script>
+  <script>
+    /* Make dynamic date appear */
+    (function () {
+      if (document.getElementById("get-current-year")) {
+        document.getElementById("get-current-year").innerHTML =
+          new Date().getFullYear();
+      }
+    })();
+    /* Function for opning navbar on mobile */
+    function toggleNavbar(collapseID) {
+      document.getElementById(collapseID).classList.toggle("hidden");
+      document.getElementById(collapseID).classList.toggle("block");
+    }
+    /* Function for dropdowns */
+    function openDropdown(event, dropdownID) {
+      let element = event.target;
+      while (element.nodeName !== "A") {
+        element = element.parentNode;
+      }
+      Popper.createPopper(element, document.getElementById(dropdownID), {
+        placement: "bottom-start"
+      });
+      document.getElementById(dropdownID).classList.toggle("hidden");
+      document.getElementById(dropdownID).classList.toggle("block");
+    }
+  </script>
 </html>
--- resources/views/dashboard/user/register.blade.php
+++ resources/views/dashboard/user/register.blade.php
@@ -5,21 +5,60 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>User Register</title>
-    <link rel="stylesheet" href="{{ asset('bootstrap.min.css') }}">    
-    <link rel="stylesheet" href="\css\loginstyles.css">
+    <link rel="stylesheet" href="{{ asset('bootstrap.min.css') }}">
+    <link rel="stylesheet" href="\css\tailwind.css">
+    <link rel="stylesheet" href="\css\index.css">
+    <link rel="stylesheet" href="\css\all.min.css">
 </head>
-<body>
+<body class="text-blueGray-700 antialiased">
+<nav
+      class="top-0 absolute z-50 w-full flex flex-wrap items-center justify-between px-2 py-3 navbar-expand-lg"
+    >
+      <div
+        class="container px-4 mx-auto flex flex-wrap items-center justify-between"
+      >
+        <div
+          class="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start"
+        >
+          <a
+            class="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white"
+            href="/"
+            >Car Policy Hub</a
+          ><button
+            class="cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
+            type="button"
+            onclick="toggleNavbar('example-collapse-navbar')"
+          >
+            <i class="text-white fas fa-bars"></i>
+          </button>
+        </div>
+       
+    </nav>
+    
+    <main>
+      <section class="relative w-full h-full py-40 min-h-screen">
+        <div
+          class="absolute top-0 w-full h-full bg-blueGray-800 bg-full bg-no-repeat"
+          style="background-image: url(/images/register_bg_2.png)"
+        ></div>
+        <div class="container mx-auto px-4 h-full">
+          <div class="flex content-center items-center justify-center h-full">
+            <div class="w-full lg:w-4/12 px-4">
+              <div
+                class="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-blueGray-200 border-0"
+              >
 
-    <div class="container">
-        <div class="row">
-            <div class="login" style="margin-top: -250px;">
-                  <h1 >Register Insurance Company</h1><hr>
+                <div class="flex-auto px-4 lg:px-10 py-10 pt-0">
+                  <div class="text-blueGray-400 text-center mb-3 font-bold">
+                    <small>User Register</small>
+                  </div>
                   <form action="{{ route('user.create') }}" method="post" autocomplete="off">
                     @if (Session::get('success'))
-                         <div class="alert alert-success">
-                             {{ Session::get('success') }}
-                         </div>
+                        <div class="alert alert-success">
+                            {{ Session::get('success') }}
+                        </div>
                     @endif
+
                     @if (Session::get('fail'))
                     <div class="alert alert-danger">
                         {{ Session::get('fail') }}
@@ -27,36 +66,152 @@
                     @endif
 
                     @csrf
-                      <div class="form-group">
-                          <label for="name" style="color: white; font-weight: bold;">Insurance Company Name</label>
-                          <input type="text" class="form-control" name="name" placeholder="Enter full name" value="{{ old('name') }}">
-                          <span class="text-danger">@error('name'){{ $message }} @enderror</span>
-                      </div>
-                      <div class="form-group">
-                        <label for="email"style="color: white; font-weight: bold;">Company Email</label>
-                        <input type="text" class="form-control" name="email" placeholder="Enter email address" value="{{ old('email') }}"required pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$">
-                        <div class="invalid-feedback">Please enter a valid email address</div>
-                        <span class="text-danger">@error('email'){{ $message }} @enderror</span>
+                    <div class="relative w-full mb-3">
+                      <label
+                        class="block uppercase text-blueGray-600 text-xs font-bold mb-2"
+                        for="name"
+                        >Insurance Company Name</label
+                      ><input
+                        type="text"
+                        name="name"
+                        class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                        placeholder="Enter full name"
+                        value="{{ old('name') }}"
+                      />
+                      <span class="text-danger">@error('name'){{ $message }} @enderror</span>
                     </div>
-                      <div class="form-group">
-                          <label for="password" style="color: white; font-weight: bold;">Password</label>
-                          <input type="password" class="form-control" name="password" placeholder="Enter password" value="{{ old('password') }}">
-                          <span class="text-danger">@error('password'){{ $message }} @enderror</span>
-                      </div>
-                      <div class="form-group">
-                        <label for="cpassword" style="color: white; font-weight: bold;">Confirm Password</label>
-                        <input type="password" class="form-control" name="cpassword" placeholder="Enter confirm password" value="{{ old('cpassword') }}">
-                        <span class="text-danger">@error('cpassword'){{ $message }} @enderror</span>
+                    <div class="relative w-full mb-3">
+                      <label
+                        class="block uppercase text-blueGray-600 text-xs font-bold mb-2"
+                        for="email"
+                        >Company Email</label
+                      ><input
+                        type="text"
+                        name="email"
+                        class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                        placeholder="Enter email address"
+                        value="{{ Session::get('verifiedEmail') ? Session::get('verifiedEmail') : old('email') }}"
+                      />
+                      <span class="text-danger">@error('email'){{ $message }}@enderror</span>
                     </div>
-                      <div class="form-group">
-                          <button type="submit" class="btn btn-primary">Register</button>
-                      </div>
-                      <br>
-                      <a href="{{ route('user.login') }}">I already have an account</a>
-                  </form>
+                    <div class="relative w-full mb-3">
+                      <label
+                        class="block uppercase text-blueGray-600 text-xs font-bold mb-2"
+                        for="password"
+                        >Password</label
+                      ><input
+                        type="password"
+                        name="password"
+                        class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                        placeholder="Enter password"
+                        value="{{ old('password') }}"
+                      />
+                      <span class="text-danger">@error('password'){{ $message }}@enderror</span>
+                    </div>
+                    <div class="relative w-full mb-3">
+                      <label
+                        class="block uppercase text-blueGray-600 text-xs font-bold mb-2"
+                        for="cpassword"
+                        >Confirm Password</label
+                      ><input
+                        type="password"
+                        name="cpassword"
+                        class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                        placeholder="Enter confirm password"
+                        value="{{ old('cpassword') }}"
+                      />
+                      <span class="text-danger">@error('cpassword'){{ $message }} @enderror</span>
+                    </div>
+                    <div class="text-center mt-6">
+                      <button
+                        class="bg-blueGray-800 text-white active:bg-blueGray-600 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full ease-linear transition-all duration-150"
+                        type="submit"
+                      >
+                        Register
+                      </button>
+                    </div>
+                    <div class="flex flex-wrap mt-6">
+                <div class="w-1/2">
+                  <a href="{{ route('user.login') }}" 
+                    ><small>I already have an account</small></a
+                  >
+                </div>
+              </div>
             </div>
+          </div>
         </div>
-    </div>
-    
-</body>
+                  </form>
+                </div>
+              </div>
+        <footer class="absolute w-full bottom-0 bg-blueGray-800 pb-6">
+          <div class="container mx-auto px-4">
+            <hr class="mb-6 border-b-1 border-blueGray-600" />
+            <div
+              class="flex flex-wrap items-center md:justify-between justify-center"
+            >
+              <div class="w-full md:w-4/12 px-4">
+                <div
+                  class="text-sm text-white font-semibold py-1 text-center md:text-left"
+                >
+                  Copyright © <span id="get-current-year"></span>
+                  <a
+                    href="/"
+                    class="text-white hover:text-blueGray-300 text-sm font-semibold py-1"
+                    >Car Policy Hub</a
+                  >
+                </div>
+              </div>
+              <div class="w-full md:w-8/12 px-4">
+                <ul
+                  class="flex flex-wrap list-none md:justify-end justify-center"
+                >
+                  <li>
+                    <a
+                      href="/"
+                      class="text-white hover:text-blueGray-300 text-sm font-semibold block py-1 px-3"
+                      >Group 1</a
+                    >
+                  </li>
+                  <li>
+                    <a
+                      href="/"
+                      class="text-white hover:text-blueGray-300 text-sm font-semibold block py-1 px-3"
+                      >About Us</a
+                    >
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </footer>
+      </section>
+    </main>
+  </body>
+  <script src="https://unpkg.com/@popperjs/core@2/dist/umd/popper.js"></script>
+  <script>
+    /* Make dynamic date appear */
+    (function () {
+      if (document.getElementById("get-current-year")) {
+        document.getElementById("get-current-year").innerHTML =
+          new Date().getFullYear();
+      }
+    })();
+    /* Function for opning navbar on mobile */
+    function toggleNavbar(collapseID) {
+      document.getElementById(collapseID).classList.toggle("hidden");
+      document.getElementById(collapseID).classList.toggle("block");
+    }
+    /* Function for dropdowns */
+    function openDropdown(event, dropdownID) {
+      let element = event.target;
+      while (element.nodeName !== "A") {
+        element = element.parentNode;
+      }
+      Popper.createPopper(element, document.getElementById(dropdownID), {
+        placement: "bottom-start"
+      });
+      document.getElementById(dropdownID).classList.toggle("hidden");
+      document.getElementById(dropdownID).classList.toggle("block");
+    }
+  </script>
 </html>
