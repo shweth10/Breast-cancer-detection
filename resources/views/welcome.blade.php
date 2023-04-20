3e7383fe5c9f152720ca4918cda98956ab9f808e
shweth10 <91576928+shweth10@users.noreply.github.com>
2023-04-21 10:37:59 +1200

    Changes to landing page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -65,6 +65,9 @@
         </style>
     </head>
     <body>
+    @php
+    $policies = App\Models\Policy::all();
+    @endphp
     
         <section class="header_area">
         <div class="navbar-area bg-white">
@@ -177,6 +180,56 @@
     </section>
 
     <!--====== HEADER PART ENDS ======-->
+
+    
+
+      <!--====== PRICING PLAN PART START ======-->
+
+      <section id="pricing" class="pricing_area pt-120 pb-120">
+        <div class="container">
+            <div class="row justify-center">
+                <div class="w-full lg:w-1/2">
+                    <div class="section_title text-center pb-6">
+                        <h5 class="sub_title">Pricing Plans</h5>
+                        <h4 class="main_title">Choose Your Plan</h4>
+                    </div> <!-- section title -->
+                </div>
+            </div> <!-- row -->
+            <div class="row">
+                <div class="w-full">
+                    
+                    <div class="pricing_content mt-6_area">
+                        <div class="tab-content">
+                            <div class="active tab-pane" id="monthlyPlan" data-tab-content>
+                                <div class="row justify-center">
+                                @foreach($policies as $policy)
+                                    <div class="w-full sm:w-9/12 md:w-7/12 lg:w-4/12">
+                                        <div class="single_pricing text-center mt-8 mx-3">
+                                            <div class="pricing_title relative inline-block">
+                                                <h4 class="title group-hover:text-white">{{ $policy->policy_type }}<br>{{ $policy->payment_period }}</h4>
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="112" height="110" viewBox="0 0 112 110">
+                                                    <path class="services_shape" id="Polygon_15" data-name="Polygon 15" d="M51.2,2.329a11,11,0,0,1,9.6,0L96.15,19.478a11,11,0,0,1,5.927,7.466l8.76,38.665a11,11,0,0,1-2.1,9.258l-24.508,30.96A11,11,0,0,1,75.6,110H36.4a11,11,0,0,1-8.625-4.173L3.266,74.867a11,11,0,0,1-2.1-9.258l8.76-38.665a11,11,0,0,1,5.927-7.466Z" fill="#f94f4f"/>
+                                                </svg>
+                                            </div>
+                                            <div class="pricing_content mt-6">
+                                                <span class="pricing_price font-bold text-black text-4xl">${{ $policy->premium_amount }}</span>
+                                                <p class="mt-4 leading-9">{{ $policy->coverage_information }} </p>
+                                                <a href="" class="main-btn pricing_btn">Choose Plan</a>
+                                            </div>
+                                        </div>  <!-- single pricing -->
+                                    </div>
+                                    @endforeach
+                                </div> <!-- row -->
+                                </div> <!-- row -->
+                            </div>
+                        </div>
+                    </div> <!-- pricing menu -->
+                </div>
+            </div> <!-- row -->
+        </div> <!-- container -->
+    </section>
+
+    <!--====== PRICING PLAN PART ENDS ======-->
 
     <!--====== SERVICES PART START ======-->
 
